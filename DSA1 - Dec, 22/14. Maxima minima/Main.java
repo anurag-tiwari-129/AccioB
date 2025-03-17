11683ce2500bfd33cd7475fd869f61d8518a0f63
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-03-17 08:47:41 +0530

    some more

---
+++ DSA1 - Dec, 22/14. Maxima minima/Main.java
@@ -0,0 +1,39 @@
+// sitaram
+// 17/03/2025
+import java.util.*;
+public class Main {
+    public static int maximaMinima(int[][] mat) {
+        // Write code here
+
+        int n = mat.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                // find minimum in row ||--------------------------------
+                int min = Integer.MAX_VALUE;
+                for(int k=0;k<n;k++)
+                    min = Math.min(min, mat[i][k]);
+
+                // find maximum in column ||-----------------------------
+                int max = Integer.MIN_VALUE;
+                for(int k=0;k<n;k++)
+                    max = Math.max(max, mat[k][j]);
+                
+                // if both are equal, return ||--------------------------
+                if(min == max)
+                    return min;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = sc.nextInt();
+        int result = maximaMinima(matrix);
+        System.out.println(result);
+        sc.close();
+    }
+}
---
+++ DSA1 - Dec, 22/15. Find the way/Main.java
@@ -0,0 +1,66 @@
+// sitaram
+// 17/03/2025
+// in class
+import java.util.*;
+public class Main {
+    public static int[] findTheWay(int[][] matrix) {
+        // Write code here
+
+        // initialization ||---------------------------------------------
+        int ans[] = new int[2];
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int i = 0, j = 0;
+        int dir = 0;
+        
+        // mouse in a matrix ||------------------------------------------
+        while(i >= 0 && i < n && j >= 0 && j < m){
+            // current location ||---------------------------------------
+            ans[0] = i;
+            ans[1] = j;
+
+            // if at 0, move forward ||----------------------------------
+            if(matrix[i][j] == 0){
+                if(dir == 0) j++; // right
+                if(dir == 1) i++; // down
+                if(dir == 2) j--; // left
+                if(dir == 3) i--; // up
+            }
+
+            // if at 1, turn right ||------------------------------------
+            else{
+                if(dir == 0){ // right to down
+                    dir = 1;
+                    i++;
+                }
+                else if(dir == 1){ // down to left
+                    dir = 2;
+                    j--;
+                }
+                else if(dir == 2){ // left to up
+                    dir = 3;
+                    i--;
+                }
+                else if(dir == 3){ // up to right
+                    dir = 0;
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = sc.nextInt();
+        int result[] = findTheWay(matrix);
+        for (int i = 0; i < result.length; i++)
+            System.out.print(result[i] + " ");
+        System.out.println();
+        sc.close();
+    }
+}
