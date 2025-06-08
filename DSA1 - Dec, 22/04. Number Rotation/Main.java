53a879edf1cb97eb72139e242b4f124b97597911
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-06-08 20:58:46 +0530

    format

--- DSA1 - Dec, 22/04. Number Rotation/Main.java
+++ DSA1 - Dec, 22/04. Number Rotation/Main.java
@@ -9,25 +9,25 @@ public class Main {
         int k = sc.nextInt();
         sc.close();
 
-        // count digits ||----------------------------------------------
+        // count digits ||-----------------------------------------------
         int count = 0, tem = n;
         while(tem > 0){
             tem /= 10;
             count++;
         }
-        // find minimum rotations ||------------------------------------
+        // find minimum rotations ||-------------------------------------
         k = k % count;
         if(k < 0)
             k = k + count;
 
-        // separate first & last ||-------------------------------------
+        // separate first & last ||--------------------------------------
         int ri_dig = 1;
         for(int i=1;i<=k;i++)
             ri_dig *= 10;
         int first = n % ri_dig;
         int last = n / ri_dig;
 
-        // adjust place of first acc. to last num ||--------------------
+        // adjust place of first acc. to last num ||---------------------
         int le_dig = 1;
         for(int i=1;i<=count-k;i++)
             le_dig *= 10;
