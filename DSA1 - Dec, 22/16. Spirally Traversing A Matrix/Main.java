ba88d3177182895b76611b8337de0a314ba6cc06
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-06-15 23:17:21 +0530

    16

--- DSA1 - Dec, 22/16. Spirally Traversing A Matrix/Main.java
+++ DSA1 - Dec, 22/16. Spirally Traversing A Matrix/Main.java
@@ -30,49 +30,49 @@ public class Main {
                     System.out.print(mat[top][j] + " ");
                 top++; // dir = 1
             }
-            // in right, top to bottom ||--------------------------------
+            // in right, top to bottom ||---------------------------------
             else if(dir == 1){
                 for(int i=top;i<=bottom;i++)
                     System.out.print(mat[i][right] + " ");
                 right--; // dir = 2
             }
-            // in bottom, right to left ||-------------------------------
+            // in bottom, right to left ||--------------------------------
             else if(dir == 2){
                 for(int j=right;j>=left;j--)
                     System.out.print(mat[bottom][j] + " ");
                 bottom--; // dir = 3
             }
-            // in left, bottom to top ||---------------------------------
+            // in left, bottom to top ||----------------------------------
             else{
                 for(int i=bottom;i>=top;i--)
                     System.out.print(mat[i][left] + " ");
                 left++; // dir = 0
             }
-            // update direction ||---------------------------------------
+            // update direction ||----------------------------------------
             dir = (dir + 1) % 4;
         }
-        // --------------------------------------------------------------
+        // ---------------------------------------------------------------
 
-        // 2nd way ||----------------------------------------------------
+        // 2nd way ||-----------------------------------------------------
         while(top <= bottom && left <= right){
-            // in top, left to right ||----------------------------------
+            // in top, left to right ||-----------------------------------
             for(int j=left;j<=right;j++){
                 System.out.print(mat[top][j] + " ");
             }
             top++;
-            // in right, top to bottom ||--------------------------------
+            // in right, top to bottom ||---------------------------------
             if(left <= right){
                 for(int i=top;i<=bottom;i++)
                     System.out.print(mat[i][right] + " ");
                 right--;
             }
-            // in bottom, right to left ||-------------------------------
+            // in bottom, right to left ||--------------------------------
             if(top <= bottom){
                 for(int j=right;j>=left;j--)
                     System.out.print(mat[bottom][j] + " ");
                 bottom--;
             }
-            // in left, bottom to top ||---------------------------------
+            // in left, bottom to top ||----------------------------------
             if(left <= right){
                 for(int i=bottom;i>=top;i--)
                     System.out.print(mat[i][left] + " ");
