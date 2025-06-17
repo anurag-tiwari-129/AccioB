b23969c83f1bbfae093f342fe44c53bb7d6f5801
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-06-17 13:48:33 +0530

    p

--- Practice/04. Null Array/Main.java
+++ Practice/04. Null Array/Main.java
@@ -6,31 +6,29 @@ public class Main{
 		// for(byte i : a_byte) 
 		// 	System.out.print(i + " ");
 		// 	// java.lang.NullPointerException
-        	// java.lang.NullPointerException
 		System.out.println("Byte : " + a_byte); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		short [] a_short = null;
 		// for(short i : a_short)
 		// 	System.out.print(i + " ");
 		// 	// java.lang.NullPointerException
-        //     // java.lang.NullPointerException
 		System.out.println("Short : " + a_short); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		int [] a_int = null;
 		// for(int i : a_int)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
+		// 	// java.lang.NullPointerException
 		System.out.println("Integer : " + a_int); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		long [] a_long = null;
 		// for(long i : a_long)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
+		// 	// java.lang.NullPointerException
 		System.out.println("Long : " + a_long); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		float [] a_float = null;
 		// for(float i : a_float)
@@ -42,35 +40,35 @@ public class Main{
 		double [] a_double = null;
 		// for(double i : a_double)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
+		// 	// java.lang.NullPointerException
 		System.out.println("Double : " + a_double); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		char [] a_char = null;
 		// for(char i : a_char)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
+		// 	// java.lang.NullPointerException
 		System.out.println("Character : " + a_char); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		boolean [] a_boolean = null;
 		// for(boolean i : a_boolean)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
+		// 	// java.lang.NullPointerException
 		System.out.println("Boolean : " + a_boolean); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		String [] a_String = null;
 		// for(String i : a_String)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
+		// 	// java.lang.NullPointerException
 		System.out.println("String : " + a_String); // null
-		// --------------------------------------------------------------
+		// -------------------------------------------------------------
 
 		Object [] a_Object = null;
 		// for(Object i : a_Object)
 		// 	System.out.print(i + " ");
-        //     // java.lang.NullPointerException
-		System.out.println("Object : " + a_Object);
+		// 	// java.lang.NullPointerException
+		System.out.println("Object : " + a_Object); // null
 	}
 }
--- Practice/05. Stable Selection Sort/Main.java
+++ Practice/05. Stable Selection Sort/Main.java
@@ -1,8 +1,8 @@
 // sitaram
 import java.util.*;
 public class Main{
-	public static void main (String[] args){
-		Scanner sc = new Scanner(System.in);
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
@@ -10,18 +10,18 @@ public class Main{
         int[] ans = SelectionSort(arr);
         for(int a : ans)
             System.out.print(a + " ");
-	}
-	public static int[] SelectionSort(int[] arr) {
+    }
+    public static int[] SelectionSort(int[] arr) {
         // Write your code here
-
+        
         int n = arr.length;
         for(int i=0;i<n-1;i++){
             int ind = i;
             for(int j=i+1;j<n;j++)
                 if(arr[ind] > arr[j])
-                    ind = j;
-
-            // here item would shift to right place, will no  direct swap
+                ind = j;
+                
+            // here item would shift to right place by not swapping directly instead it will shift one by one
             // so that, it makes code stable i.e., stable sort
             for(int k=ind;k>i;k--)
                 if(arr[k] < arr[k - 1]){
