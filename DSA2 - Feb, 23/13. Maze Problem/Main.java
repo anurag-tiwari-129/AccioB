385dfa14cb824e98382d951029b0407f31d5c2d2
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-07-26 15:58:49 +0530

    dsa 2

--- DSA2 - Feb, 23/13. Maze Problem/Main.java
+++ DSA2 - Feb, 23/13. Maze Problem/Main.java
@@ -3,25 +3,26 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-	public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
         // Write your code here
-
-		if(sr == dr && sc == dc){
-			System.out.println(psf);
-			return;
-		}
+        if(sr == dr && sc == dc){
+            System.out.println(psf);
+            return;
+        }
         // can skip bcz of for conditions
-		if(sr > dr || sc > dc) return;
-
-		// move horizontal ||---------------------------------------------
-		for(int i=1;i<=dc-sc;i++)
-			printMazePaths(sr, sc + i, dr, dc, psf + 'h' + i);
-		// move vertical ||-----------------------------------------------
-		for(int i=1;i<=dr-sr;i++)
-			printMazePaths(sr + i, sc, dr, dc, psf + 'v' + i);
-		// move diagonal ||-----------------------------------------------
-		for(int i=1;i<=Math.min(dc-sc, dr-sr);i++)
-			printMazePaths(sr + i, sc + i, dr, dc, psf + 'd' + i);
+        if(sr > dr || sc > dc) return;
+        
+        // move horizontal ||---------------------------------------------
+        for(int i=1;i<=dc-sc;i++)
+            printMazePaths(sr, sc + i, dr, dc, psf + 'h' + i);
+        
+        // move vertical ||-----------------------------------------------
+        for(int i=1;i<=dr-sr;i++)
+            printMazePaths(sr + i, sc, dr, dc, psf + 'v' + i);
+        
+        // move diagonal ||-----------------------------------------------
+        for(int i=1;i<=Math.min(dc-sc, dr-sr);i++)
+            printMazePaths(sr + i, sc + i, dr, dc, psf + 'd' + i);
     }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
