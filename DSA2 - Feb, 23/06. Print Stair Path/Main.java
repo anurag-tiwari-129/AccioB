a6269b741aaa92633865a9fa48f02319714b717a
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-09-16 06:54:41 +0530

    dsa2

--- DSA2 - Feb, 23/06. Print Stair Path/Main.java
+++ DSA2 - Feb, 23/06. Print Stair Path/Main.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static void printStairPaths(int n,String pathSoFar) {
+    static void printStairPaths(int n, String pathSoFar) {
         // Write your code here
 
         if(n < 0) return;
@@ -19,6 +19,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printStairPaths(n,"");
+        printStairPaths(n, "");
     }
 }
