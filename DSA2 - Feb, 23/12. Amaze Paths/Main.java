6a088a6cc90edec207a384360a972c3735039d1e
Anurag Tiwari <anurag.tiwari.11299@gmail.com>
2025-09-16 23:39:54 +0530

    dsa2

--- DSA2 - Feb, 23/12. Amaze Paths/Main.java
+++ DSA2 - Feb, 23/12. Amaze Paths/Main.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void aMazePaths(int n, int m, String psf, int i , int j){
+    public static void aMazePaths(int n, int m, String psf, int i, int j){
         // Write your code here
         
         if(i == n && j == m){
@@ -17,10 +17,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputLine;
-        inputLine = br.readLine().trim().split(" ");
+        inputLine = br.readLine().trim().split(" "); // new array
         int n = Integer.parseInt(inputLine[0]);
-        inputLine = br.readLine().trim().split(" ");
+        inputLine = br.readLine().trim().split(" "); // new array, overwrite old one
         int m = Integer.parseInt(inputLine[0]);
-        aMazePaths(n, m, "",1,1);
+        aMazePaths(n, m, "", 1, 1);
     }
 }
